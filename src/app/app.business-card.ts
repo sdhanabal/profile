dc520b2ae4d723170bbc6136ba8ee5d96c4524b2
Suravam <kumar.dsaravana@gmail.com>
2020-08-26 00:51:39 +0530

    Include grid list

--- src/app/app.business-card.ts
+++ src/app/app.business-card.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -6,10 +6,18 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './app.business-card.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
   mailText:string = "";
   isActive:boolean = false;
+  public innerWidth: any;
+  public gen_width = 550;
+  ngOnInit() {
+      this.innerWidth = window.innerWidth;
+      if (this.innerWidth < this.gen_width){
+        this.gen_width = this.innerWidth;
+      }      
+  }
   send_mail(){
     this.toggle();
     var mail = 'mailto:kumar.dsaravana@gmail.com?subject=Interested to connect with you&body=Hi Saravana';
--- src/app/app.showcase.ts
+++ src/app/app.showcase.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { MatDialogRef } from "@angular/material/dialog";
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'showcase',
   templateUrl: './app.showcase.html',
@@ -14,10 +20,38 @@ export class ShowcaseComponent implements OnInit{
   isActive:boolean = false;
   panelOpenState = true;
   tab_bg = this.rgbToHex(241, 236, 236)
-
+  public innerWidth: any;
+  public gen_width = 550;
   constructor(public dialog: MatDialog) {}
-
+  telecom: Tile[] = [
+    {text: 'SDWAN', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'SS7', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'LTE', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Telecom', cols: 3, rows: 1, color: 'lightblue'}
+  ];
+  automation: Tile[] = [
+    {text: 'Python', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Shell', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Ansible', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Robot Framework', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Automation Development', cols: 4, rows: 1, color: 'lightblue'}
+  ];
+  web: Tile[] = [
+    {text: 'Angular', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'JQuery', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'HTML CSS Bootstrap', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Python', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'NodeJS', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'MongoDB', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'SPA / AJAX', cols: 3, rows: 1, color: 'lightgrey'},
+    {text: 'REST', cols: 3, rows: 1, color: 'lightgreen'},
+    {text: 'Full Stack Web App Development', cols: 6, rows: 1, color: 'lightblue'}    
+  ];
   ngOnInit() {
+      this.innerWidth = window.innerWidth;
+      if (this.innerWidth < this.gen_width){
+        this.gen_width = this.innerWidth;
+      }
   }
   // events
   public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
@@ -30,6 +64,7 @@ export class ShowcaseComponent implements OnInit{
   openDialog() {
     this.dialog.open(HybridDetails);
   }
+
   componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
